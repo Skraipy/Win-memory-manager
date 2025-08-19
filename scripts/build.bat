7028dd91b45879fd6e4851cb8dc31bcc268d841d
Skraipy <78777339+Skraipy@users.noreply.github.com>
2025-08-19 14:41:29 +0300

    Update build.bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,7 +1,9 @@
 @echo off
+cd ..
 if not exist "build" mkdir build
 cd build
 cmake ..
 cmake --build .
-echo The assembly is complete! the executable file is located in the folder build/Release/memory_manager.exe
+echo The assembly is complete! 
+echo the executable file is located in the folder build/Release/memory_manager.exe
 pause
